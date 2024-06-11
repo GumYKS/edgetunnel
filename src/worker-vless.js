dc63c6da22265f0df5ddf46121d184838eed7704
GumYKS <brilliantonlineservice@gmail.com>
2024-06-11 16:07:05 +0800

    Update worker-vless.js

--- src/worker-vless.js
+++ src/worker-vless.js
@@ -4,9 +4,9 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '2574c445-79de-4041-a4ae-b11faa16ad14';
 
-let proxyIP = '';
+let proxyIP = 'workers.cloudflare.cyou,cdn.xn--b6gac.eu.org,cdn-all.xn--b6gac.eu.org,bestproxy.onecf.eu.org';
 
 
 if (!isValidUUID(userID)) {
